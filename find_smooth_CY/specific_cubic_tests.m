de4e8b23e455b510b9a339877bd3f9a6fab5408d
danielplatt <d.platt@web.de>
2022-08-05 14:55:23 +0100

    Add computation to confirm that specific cubic has b1 not 0

--- find_smooth_CY/specific_cubic_tests.m
+++ find_smooth_CY/specific_cubic_tests.m
@@ -1,12 +1,12 @@
 Q:=RationalField();
 P5<x,y,z,u,v> := ProjectiveSpace(Q,4);
-cubic:=x*(y^2+z^2+u^2+v^2)-(y^3+z^3+u^3-(1/2)*v^3);
+cubic:=x*(y^2+z^2+u^2-v^2)-(y^3+z^3+u^3-(1/2)*v^3);
 V:=Scheme(P5,cubic);
 cubic;
 print "The variety is smooth: ";
 IsNonsingular(V);
 
-smoothing:=cubic+(1/4)*x^3;
+smoothing:=cubic-(1/4)*x^3;
 W:=Scheme(P5,smoothing);
 smoothing;
 print "The variety is smooth: ";
