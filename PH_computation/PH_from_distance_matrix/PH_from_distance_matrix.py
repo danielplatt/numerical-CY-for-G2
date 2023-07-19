d50f9f69b2c9486500d4539c996e2f5303f070ca
danielplatt <d.platt@web.de>
2023-07-19 14:11:16 +0100

    Add vanilla PH results

---
+++ PH_computation/PH_from_distance_matrix/PH_from_distance_matrix.py
@@ -0,0 +1,51 @@
+import numpy as np
+from gph import ripser_parallel
+import time
+from itertools import combinations
+from tqdm import tqdm
+from gtda.plotting import plot_diagram
+from gtda.homology._utils import _postprocess_diagrams
+
+
+def load_as_np():
+    file = open('vanilla_modified_distances.csv', 'rb')
+    data = np.loadtxt(file, delimiter=",", dtype=np.float64)
+    print(data.shape, data.dtype)
+    return data
+
+def explore_data(data):
+    print(f'Number of points: {len(data)}')
+
+    closest_point_distances = []
+    average_point_distances = []
+    check_how_many = 50
+    for k in tqdm(range(check_how_many)):
+        point_k_distances = []
+        for l in range(len(data)):
+            if k != l:
+                point_k_distances += [np.linalg.norm(data[k] - data[l])]
+        closest_point_distances += [np.min(point_k_distances)]
+        average_point_distances += [np.average(point_k_distances)]
+
+    print(f'Average distance to closest point: {np.average(closest_point_distances)}')
+    print(f'Min distance to closest point: {np.min(closest_point_distances)}')
+    print(f'Max distance to closest point: {np.max(closest_point_distances)}')
+    print(f'Average distance between two points: {np.average(average_point_distances)}')
+
+def main(data, thresh):
+    start_time = time.time()
+    dgm = ripser_parallel(data, metric="precomputed", thresh=thresh, maxdim=2, n_threads=-1)
+
+    dgm_gtda = _postprocess_diagrams([dgm["dgms"]], "ripser", (0, 1), np.inf, True)[0]
+    plot_diagram(dgm_gtda, homology_dimensions=(0, 1)).show()
+
+    np.set_printoptions(threshold=np.inf)
+    print(dgm)
+    print(f'Threshold: {thresh}. Computation time: {time.time() - start_time}')
+
+
+if __name__ == '__main__':
+    data = load_as_np()
+    thresh = 3*0.1182402879390776
+    # explore_data(data)
+    main(data, thresh)
